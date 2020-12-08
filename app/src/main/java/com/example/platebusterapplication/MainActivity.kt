46b834200f477f01168111061cd27e06eb3a1645
IshaBansal0408 <isha.bansal1504@gmail.com>
2020-12-08 18:03:29 +0530

    first draft

---
+++ app/src/main/java/com/example/platebusterapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.platebusterapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun CreateAccount(view: View) {
+        val button=findViewById<Button>(R.id.button2)
+        button.setOnClickListener{
+            val intent= Intent(this,createaccount::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/platebusterapplication/createaccount.kt
@@ -0,0 +1,11 @@
+package com.example.platebusterapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class createaccount : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_createaccount)
+    }
+}

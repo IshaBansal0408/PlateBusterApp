96ed0360f51a0f853d21d04b13962025d0069603
IshaBansal0408 <isha.bansal1504@gmail.com>
2021-04-22 18:08:15 +0530

    new activity added

---
+++ app/src/main/java/com/example/platebuster/Screen_Main.java
@@ -0,0 +1,14 @@
+package com.example.platebuster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Screen_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen__main);
+    }
+}

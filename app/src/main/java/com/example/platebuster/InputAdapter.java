d5b728c90ac890cc2dc3b3b5b28e9d830b49e727
IshaBansal0408 <isha.bansal1504@gmail.com>
2021-04-23 23:59:52 +0530

    updating main screen 3

---
+++ app/src/main/java/com/example/platebuster/InputAdapter.java
@@ -0,0 +1,34 @@
+package com.example.platebuster;
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class InputAdapter extends FragmentPagerAdapter {
+    private Context context;
+    int totalTabs;
+
+    public InputAdapter(FragmentManager fm, Context context, int totalTabs){
+        super(fm);
+        this.context=context;
+        this.totalTabs=totalTabs;
+    }
+
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+
+    public Fragment getItems(int position){
+        switch (position){
+            case 0:
+                input_tab_fragment input_tab_fragment = new input_tab_fragment();
+                return input_tab_fragment;
+            default:
+                return null;
+        }
+    }
+
+}
--- app/src/main/java/com/example/platebuster/Screen_Main.java
+++ app/src/main/java/com/example/platebuster/Screen_Main.java
@@ -1,14 +1,45 @@
 package com.example.platebuster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
 public class Screen_Main extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    FloatingActionButton cam,up;
+    float v=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen__main);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+        cam = findViewById(R.id.float_camera);
+        up = findViewById(R.id.float_upload);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Enter data"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final InputAdapter adapter = new InputAdapter(getSupportFragmentManager(),this,tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        cam.setTranslationY(300);
+        up.setTranslationY(300);
+
+        cam.setAlpha(v);
+        up.setAlpha(v);
+
+        cam.animate()
+
     }
 }

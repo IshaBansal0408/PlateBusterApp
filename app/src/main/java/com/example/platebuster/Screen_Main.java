a5ce78486dafa40773b580221e35bb4ffa6aee78
IshaBansal0408 <isha.bansal1504@gmail.com>
2021-06-03 18:45:11 +0530

    camera feature added

--- app/src/main/java/com/example/platebuster/Screen_Main.java
+++ app/src/main/java/com/example/platebuster/Screen_Main.java
@@ -1,49 +1,106 @@
 package com.example.platebuster;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.tabs.TabLayout;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Screen_Main extends AppCompatActivity {
+    public  static final int RequestPermissionCode  = 1 ;
+    FloatingActionButton cam;
+    Button get_ans;
+    EditText editText;
+    ImageView inp_img;
+    Intent intent;
+    String currentPhotoPath;
+    float v = 0;
 
-    TabLayout tabLayout;
-    ViewPager viewPager;
-    FloatingActionButton cam,up;
-    float v=0;
-
+    @Nullable
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen__main);
-
-        tabLayout = findViewById(R.id.tab_layout);
-        viewPager = findViewById(R.id.view_pager);
+        editText = findViewById(R.id.input);
+        inp_img = findViewById(R.id.image_input);
         cam = findViewById(R.id.float_camera);
-        up = findViewById(R.id.float_upload);
+        get_ans = findViewById(R.id.get_num);
 
-        tabLayout.addTab(tabLayout.newTab().setText("Enter data"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        final InputAdapter adapter = new InputAdapter(getSupportFragmentManager(),this,tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
-
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-
+        editText.getText().clear();
         cam.setTranslationY(300);
-        up.setTranslationY(300);
-        tabLayout.setTranslationY(300);
-
+        get_ans.setTranslationY(300);
+        get_ans.setAlpha(v);
         cam.setAlpha(v);
-        up.setAlpha(v);
-        tabLayout.setAlpha(v);
-
         cam.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
-        up.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(600).start();
-        tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(100).start();
+        get_ans.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
 
+        EnableRuntimePermission();
+        cam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 7);
+            }
+        });
+
+        get_ans.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                detectTextFromImage();
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 7 && resultCode == RESULT_OK) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            inp_img.setImageBitmap(bitmap);
+        }
+    }
+    public void EnableRuntimePermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(Screen_Main.this,
+                Manifest.permission.CAMERA))
+        {
+            Toast.makeText(Screen_Main.this,"CAMERA permission allows us to Access CAMERA app", Toast.LENGTH_LONG).show();
+        } else {
+
+            ActivityCompat.requestPermissions(Screen_Main.this,new String[]{
+                    Manifest.permission.CAMERA}, RequestPermissionCode);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int RC, String per[], int[] PResult) {
+        switch (RC) {
+            case RequestPermissionCode:
+                if (PResult.length > 0 && PResult[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(Screen_Main.this,"Permission Granted, Now your application can access CAMERA.", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(Screen_Main.this,"Permission Canceled, Now your application cannot access CAMERA.", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
     }
 }
